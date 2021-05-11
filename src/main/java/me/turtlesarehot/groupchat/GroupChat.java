7c96569cb6ba8c6de3ab0b1f55e31955ec218756
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-05-11 13:26:19 -0400

    Fix spacing

--- src/main/java/me/turtlesarehot/groupchat/GroupChat.java
+++ src/main/java/me/turtlesarehot/groupchat/GroupChat.java
@@ -1,15 +1,12 @@
-package me.turtlesarehot.groupchat;
-
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.plugin.Plugin;
-
-import java.util.HashSet;
-import java.util.UUID;
-
-public class GroupChat extends Plugin {
-
-    @Override
-    public void onEnable() {
-        ProxyServer.getInstance().getPluginManager().registerCommand(this, new GCCommand());
-    }
-}
+package me.turtlesarehot.groupchat;
+
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Plugin;
+
+
+public class GroupChat extends Plugin {
+    @Override
+    public void onEnable() {
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, new GCCommand());
+    }
+}
