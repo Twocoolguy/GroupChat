c8d40167005d02b9ddd48b2ba727ba7f291bd7af
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-05-13 15:01:43 -0400

    Create PlayerChat.java

---
+++ src/main/java/me/turtlesarehot/groupchat/PlayerChat.java
@@ -0,0 +1,21 @@
+package me.turtlesarehot.groupchat;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.ChatEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+public class PlayerChat implements Listener {
+
+    @EventHandler
+    public void onChat(ChatEvent event) {
+        if(event.getSender() instanceof ProxiedPlayer) {
+            ProxiedPlayer sender = (ProxiedPlayer) event.getSender();
+            GroupPlayer gp = GroupCommand.getGroupPlayer(sender.getUniqueId());
+            if(gp != null && gp.isChatToggled()) {
+                gp.getGroup().sendMessage(ChatColor.GREEN + "[Group] " + ChatColor.YELLOW + sender.getDisplayName() + ": " + event.getMessage());
+            }
+        }
+    }
+}
