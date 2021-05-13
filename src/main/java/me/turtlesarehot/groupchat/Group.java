9e9f6b75b444e14d8c58e006f5487c3d834cba05
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-05-13 15:17:16 -0400

    Update Group.java

--- src/main/java/me/turtlesarehot/groupchat/Group.java
+++ src/main/java/me/turtlesarehot/groupchat/Group.java
@@ -12,21 +12,21 @@ import java.util.UUID;
 
 public class Group {
 
-    private GroupPlayer leader;
-    private ArrayList<GroupPlayer> members;
+    private UUID leader;
+    private ArrayList<UUID> members;
     private int id;
 
-    public Group(GroupPlayer leader, int id) {
+    public Group(UUID leader, int id) {
         this.leader = leader;
-        this.members = new ArrayList<GroupPlayer>();
+        this.members = new ArrayList<UUID>();
         this.id = id;
     }
 
     // Returns the UUID of the leader of the group.
-    public GroupPlayer getLeader() { return this.leader; }
+    public UUID getLeader() { return this.leader; }
 
     // Returns the UUIDs of the members of the group (doesn't include leader).
-    public ArrayList<GroupPlayer> getMembers() { return this.members; }
+    public ArrayList<UUID> getMembers() { return this.members; }
 
     // Gets the group ID.
     public int getId() { return this.id; }
@@ -35,15 +35,13 @@ public class Group {
     public int memberCount() { return this.members.size() + 1; }
 
     // Sets the leader to this new one.
-    public void setLeader(GroupPlayer leader) {
+    public void setLeader(UUID leader) {
         this.leader = leader;
     }
 
 
     // Adds a member to the group.
-    public void addMember(GroupPlayer member) {
-        this.members.add(member);
-    }
+    public void addMember(UUID member) { this.members.add(member); }
 
     // Removes a member from the group.
     public void removeMember(GroupPlayer member) {
@@ -58,13 +56,8 @@ public class Group {
         for (Map.Entry<String, ServerInfo> entry : servers.entrySet()) {
             allPlayers.addAll(entry.getValue().getPlayers());
         }
-        ArrayList<UUID> groupUUIDs = new ArrayList<UUID>();
-        groupUUIDs.add(this.leader.getUUID());
-        for (int h = 0; h < this.members.size(); h++) {
-            groupUUIDs.add(this.members.get(h).getUUID());
-        }
         for(int i = 0; i < allPlayers.size(); i++) {
-            if(groupUUIDs.contains(allPlayers.get(i).getUniqueId())) {
+            if(this.members.contains(allPlayers.get(i).getUniqueId())) {
                 allPlayers.get(i).sendMessage(allPlayers.get(i).getUniqueId(), new TextComponent(message));
             }
         }
