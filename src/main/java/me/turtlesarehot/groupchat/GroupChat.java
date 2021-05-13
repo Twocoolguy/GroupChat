812a8fabb741c37aabfb34a5a465bed225992105
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-05-13 15:01:01 -0400

    Update GroupChat.java

--- src/main/java/me/turtlesarehot/groupchat/GroupChat.java
+++ src/main/java/me/turtlesarehot/groupchat/GroupChat.java
@@ -8,5 +8,6 @@ public class GroupChat extends Plugin {
     @Override
     public void onEnable() {
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new GCCommand());
+        ProxyServer.getInstance().getPluginManager().registerListener(this, new PlayerChat());
     }
 }
