785178020b5b46c0a07c864d4232a662f372f945
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-05-13 17:24:50 -0400

    Update PlayerChat.java

--- src/main/java/me/turtlesarehot/groupchat/PlayerChat.java
+++ src/main/java/me/turtlesarehot/groupchat/PlayerChat.java
@@ -11,10 +11,13 @@ public class PlayerChat implements Listener {
     @EventHandler
     public void onChat(ChatEvent event) {
         if(event.getSender() instanceof ProxiedPlayer) {
-            ProxiedPlayer sender = (ProxiedPlayer) event.getSender();
-            GroupPlayer gp = GroupCommand.getGroupPlayer(sender.getUniqueId());
-            if(gp != null && gp.isChatToggled()) {
-                GroupCommand.getGroupById(gp.getGroup()).sendMessage(ChatColor.GREEN + "[Group] " + ChatColor.YELLOW + sender.getDisplayName() + ": " + event.getMessage());
+            if(event.getMessage().charAt(0) != '/') {
+                ProxiedPlayer sender = (ProxiedPlayer) event.getSender();
+                GroupPlayer gp = GroupCommand.getGroupPlayer(sender.getUniqueId());
+                if (gp != null && gp.isChatToggled()) {
+                    GroupCommand.getGroupById(gp.getGroup()).sendMessage(ChatColor.GREEN + "[Group] " + ChatColor.YELLOW + sender.getDisplayName() + ": " + event.getMessage());
+                    event.setCancelled(true);
+                }
             }
         }
     }
